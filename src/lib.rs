7870d0ccfb9fad7aacc39e1a63fc8e9940caccc0
mfragger <58777037+mfragger@users.noreply.github.com>
2024-05-31 22:08:59 +0800

    add second number into stream

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,8 @@ pub struct BitPack {
 
 struct BitInfo {
     bit_type: BitType,
-    starting_bit_number_inc: u8,
-    ending_bit_number_exc: u8,
+    starting_bit_position_inc: u8,
+    ending_bit_position_exc: u8,
     starting_array_index_inc: u8,
     ending_array_index_exc: u8,
 }
@@ -32,9 +32,9 @@ impl Construct for BitInfo {
         BitInfo {
             bit_type: BitType::U8(0),
             ending_array_index_exc: 0,
-            ending_bit_number_exc: 0,
+            ending_bit_position_exc: 0,
             starting_array_index_inc: 0,
-            starting_bit_number_inc: 0,
+            starting_bit_position_inc: 0,
         }
     }
 }
@@ -50,7 +50,7 @@ impl Construct for BitPack {
 }
 
 impl BitPack {
-    pub fn set(mut self, name: &str, size: u8, initial_value: u8) {
+    pub fn set(&mut self, name: &str, size: u8, initial_value: u8) {
         
         let size_type: BitType = match size {
             1..=8 => BitType::U8(0),
@@ -61,34 +61,47 @@ impl BitPack {
             _ => panic!("Value out of range"),
         };
 
-        let value_type: BitType = match 128 - initial_value.leading_zeros() {
-            1..=8 => BitType::U8(0),
-            9..=16 => BitType::U16(0),
-            17..=32 => BitType::U32(0),
-            33..=64 => BitType::U16(0),
-            65..=128 => BitType::U128(0),
-            _ => panic!("Value out of range"),
-        };
-
         //check space for size (mask_type)
-        let last_element = self.bit_info.last().unwrap_or(&BitInfo {
+        let last_bit_info = self.bit_info.last().unwrap_or(&BitInfo {
             bit_type : BitType::U8(0),
-            starting_bit_number_inc : 0,
+            starting_bit_position_inc : 0,
             ending_array_index_exc : 0,
-            ending_bit_number_exc : 0,
+            ending_bit_position_exc : 0,
             starting_array_index_inc : 0,
         });
 
-        if last_element.starting_bit_number_inc == last_element.ending_bit_number_exc {
-            let mut new_bit_info : BitInfo = BitInfo::new();
-            new_bit_info.starting_bit_number_inc = last_element.ending_bit_number_exc;
-            new_bit_info.ending_bit_number_exc = last_element.ending_bit_number_exc + size + 1;
-            new_bit_info.starting_array_index_inc = last_element.ending_array_index_exc;
-            new_bit_info.ending_array_index_exc = last_element.ending_bit_number_exc + 1;
-            self.bit_info.push(new_bit_info);
-        }
+        let mut new_bit_info : BitInfo = BitInfo::new();
+        if last_bit_info.starting_bit_position_inc == last_bit_info.ending_bit_position_exc {
+            //If we have no data
+            new_bit_info.starting_bit_position_inc = last_bit_info.ending_bit_position_exc;
+            new_bit_info.ending_bit_position_exc = last_bit_info.ending_bit_position_exc + size + 1;
+            new_bit_info.starting_array_index_inc = last_bit_info.ending_array_index_exc;
+            new_bit_info.ending_array_index_exc = last_bit_info.ending_array_index_exc + 1;
 
-        self.bytes.push(self.bytes.last().unwrap_or(&0) | initial_value as u8);
+            self.bytes.push(self.bytes.last().unwrap_or(&0) | initial_value);
+
+        } else {
+            //if we have data
+            //shift to left based on last info
+            let shifted_value = initial_value << (last_bit_info.ending_bit_position_exc - 1);
+            let new_value = self.bytes.last().unwrap_or(&0) | shifted_value;
+             
+            self.bytes[last_bit_info.starting_array_index_inc as usize] = new_value;
+            
+            new_bit_info.starting_bit_position_inc = last_bit_info.ending_bit_position_exc;
+                
+            let new_ending_bit_position_exc = last_bit_info.ending_bit_position_exc + size;
+
+            new_bit_info.ending_bit_position_exc = if new_ending_bit_position_exc > 8 {
+                0
+            } else {
+                new_ending_bit_position_exc
+            };
+
+            new_bit_info.starting_array_index_inc = 0;
+            new_bit_info.ending_array_index_exc = 1;
+        }
+        self.bit_info.push(new_bit_info);
         self.bit_find.insert(name.to_owned(), (self.bit_info.len() - 1).try_into().unwrap());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,26 @@
 use rs_custom_bit_width_packer::{BitPack, BitType, Construct};
 
 fn main () {
-    // const LOW_3_BITS_MASK: u8 = 0b0000_0000;
-    // let num = 255u8;
-    // let high = num >> 4; 
+    let mut bit_pack = BitPack::new();
+    bit_pack.set("Hello", 4, 7);
+    bit_pack.set("World", 4, 6);
+    
+    
+    //0b[0110][0111]
+    
+    //0b0000_0111
+    //OR  0b0000_0110
+//-----------------
+    //0b0000_0111
+// 0000_0000_0110
+    bit_pack.set("Zack", 4, 6);
 
-
-    // let sam1 = 15u8;
-    // let sam2 = 15u128;
-
-    // // println!("{}", u8::from(high & LOW_3_BITS_MASK));
-    // println!("How many in u8: {}, How many in u128 {}", sam1.leading_zeros(), sam2.leading_zeros());
-
-    // let initial_value: u128 = 0b0000_0011;
-    // // 0b0000_0[011];
-    // // 0b0000_0[011]  0b1111_1111
-    // // 0b0000_0[011]
-    // // 0b0000_0000;
-
-    // let mask_type: BitType = match 128 - initial_value.leading_zeros() {
-    //     1..=8 => BitType::U8(0),
-    //     9..=16 => BitType::U16(0),
-    //     17..=32 => BitType::U32(0),
-    //     33..=64 => BitType::U16(0),
-    //     65..=128 => BitType::U128(0),
-    //     _ => panic!("Value out of range"),
-    // };
-    // println!("{},{:?}", 128-initial_value.leading_zeros(), mask_type);
-
-    let bit_pack = BitPack::new();
-    bit_pack.set("Hello", 4, 15);
-
+    //0b[0000][0111]_0b[0110][0111]
+    
+    //0b0000_0111
+    //OR  0b0000_0110
+//-----------------
+    //0b0000_0111
+// 0000_0000_0110
 
 }
-//Declare a size ==== 3
-    //store mask = 0b0000_0111
-    // 0b0000_0333
-    //Set a pointer = 0
-    //set NEXT POINTER = 4
-//Set a num with size 4
-    //Store mask =1 0b000_111
-    //0b0111_1333
-    //bit shift to === 3
-    //set a pointer = 4
-    //set NEXT pointer = 7
-//Declare a size === 3
-    //0b000_0022_0b2111_1333
-    //set pointer = 8
-    //store mask = 0b000_0111
-    //set next pointer = 3
