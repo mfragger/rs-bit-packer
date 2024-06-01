8900fdd0f88d20371422e0a47e4a2b6d765eb6ea
mfragger <58777037+mfragger@users.noreply.github.com>
2024-06-02 01:32:00 +0800

    passed 3 data test

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
-use std::collections::HashMap;
+use core::num;
+use std::{collections::HashMap, ops::BitOr};
 
+#[derive(PartialEq)]
 pub struct BitPack {
     bytes : Vec<u8>,
     bit_info: Vec<BitInfo>,
     bit_find: HashMap<String, u8>,
 }
 
+#[derive(PartialEq, Clone, Debug)]
 struct BitInfo {
     bit_type: BitType,
     starting_bit_position_inc: u8,
@@ -14,7 +17,7 @@ struct BitInfo {
     ending_array_index_exc: u8,
 }
 
-#[derive(Debug)]
+#[derive(PartialEq, Clone, Debug)]
 pub enum BitType  {
     U8(u8),
     U16(u16),
@@ -50,58 +53,224 @@ impl Construct for BitPack {
 }
 
 impl BitPack {
-    pub fn set(&mut self, name: &str, size: u8, initial_value: u8) {
+    pub fn set(&mut self, name: &str, to_alloc_size: u8, initial_value: u8) {
         
-        let size_type: BitType = match size {
-            1..=8 => BitType::U8(0),
-            9..=16 => BitType::U16(0),
-            17..=32 => BitType::U32(0),
-            33..=64 => BitType::U16(0),
-            65..=128 => BitType::U128(0),
-            _ => panic!("Value out of range"),
-        };
+        // let size_type: BitType = match size {
+        //     1..=8 => BitType::U8(0),
+        //     9..=16 => BitType::U16(0),
+        //     17..=32 => BitType::U32(0),
+        //     33..=64 => BitType::U16(0),
+        //     65..=128 => BitType::U128(0),
+        //     _ => panic!("Value out of range"),
+        // };
 
-        //check space for size (mask_type)
-        let last_bit_info = self.bit_info.last().unwrap_or(&BitInfo {
-            bit_type : BitType::U8(0),
-            starting_bit_position_inc : 0,
-            ending_array_index_exc : 0,
-            ending_bit_position_exc : 0,
-            starting_array_index_inc : 0,
-        });
+        let binding = BitInfo::new();
+        let last_bit_info: BitInfo = self.bit_info.last().unwrap_or(&binding).to_owned();
 
-        let mut new_bit_info : BitInfo = BitInfo::new();
-        if last_bit_info.starting_bit_position_inc == last_bit_info.ending_bit_position_exc {
-            //If we have no data
-            new_bit_info.starting_bit_position_inc = last_bit_info.ending_bit_position_exc;
-            new_bit_info.ending_bit_position_exc = last_bit_info.ending_bit_position_exc + size + 1;
-            new_bit_info.starting_array_index_inc = last_bit_info.ending_array_index_exc;
-            new_bit_info.ending_array_index_exc = last_bit_info.ending_array_index_exc + 1;
+        self.bit_info.push(Self::new_bit_info(&last_bit_info, to_alloc_size, self.bytes.len()));
 
-            self.bytes.push(self.bytes.last().unwrap_or(&0) | initial_value);
-
+        if (to_alloc_size + last_bit_info.starting_bit_position_inc) > 8 || last_bit_info.starting_bit_position_inc == last_bit_info.ending_bit_position_exc {
+            self.bytes.push(initial_value);
         } else {
-            //if we have data
-            //shift to left based on last info
-            let shifted_value = initial_value << (last_bit_info.ending_bit_position_exc - 1);
-            let new_value = self.bytes.last().unwrap_or(&0) | shifted_value;
-             
-            self.bytes[last_bit_info.starting_array_index_inc as usize] = new_value;
-            
-            new_bit_info.starting_bit_position_inc = last_bit_info.ending_bit_position_exc;
-                
-            let new_ending_bit_position_exc = last_bit_info.ending_bit_position_exc + size;
-
-            new_bit_info.ending_bit_position_exc = if new_ending_bit_position_exc > 8 {
-                0
-            } else {
-                new_ending_bit_position_exc
-            };
-
-            new_bit_info.starting_array_index_inc = 0;
-            new_bit_info.ending_array_index_exc = 1;
+            let last_index = self.bytes.len() - 1;
+            let _last_index_u8 = last_index as u8;
+            for i in (_last_index_u8)..last_bit_info.ending_array_index_exc {
+                let new_value: u8;
+                match i {
+                    last_index => {
+                        new_value = initial_value << to_alloc_size;
+                        self.bytes[last_index as usize] = self.bytes[last_index as usize] | new_value;
+                    }
+                    _ => {
+                        new_value = initial_value >> to_alloc_size * i;
+                        self.bytes.push(new_value);
+                    }
+                }
+            }
         }
-        self.bit_info.push(new_bit_info);
+        
         self.bit_find.insert(name.to_owned(), (self.bit_info.len() - 1).try_into().unwrap());
     }
+
+    pub fn get_bit_info (&self, index : usize) -> &BitInfo {
+        &self.bit_info[index]
+    }
+
+    fn new_bit_info (last_bit_info : &BitInfo, to_alloc_size: u8, array_size: usize) -> BitInfo {
+
+        let is_first_run = array_size == 0;
+
+        let mut num_allocate = 0;
+        let mut space_left = 0;
+        if last_bit_info.ending_bit_position_exc != 0 {
+            space_left = (last_bit_info.ending_bit_position_exc.abs_diff(1)).checked_sub(8).unwrap_or(to_alloc_size + 1);
+        }
+
+        if space_left < to_alloc_size {
+            num_allocate = ((to_alloc_size - space_left) + 7) /8;
+        } else if !is_first_run && last_bit_info.ending_array_index_exc == 0 {
+            num_allocate = 1;
+
+        }
+
+        let new_ending_bit_postion = last_bit_info.ending_bit_position_exc + to_alloc_size; 
+        
+        let bit_info = BitInfo {
+            bit_type: BitType::U8(0),
+            starting_bit_position_inc: last_bit_info.ending_bit_position_exc,
+            ending_bit_position_exc: if new_ending_bit_postion > 8 { 
+                    0 
+                } else { 
+                    new_ending_bit_postion 
+                }
+                + if last_bit_info.ending_bit_position_exc == 0 {
+                    1
+                } else {
+                    0
+                },
+            starting_array_index_inc: if is_first_run {
+                0
+            } else {
+                if num_allocate == 0 {
+                    last_bit_info.starting_array_index_inc
+                } else {
+                    last_bit_info.starting_array_index_inc + 1
+                }
+            },
+            ending_array_index_exc: if is_first_run {
+                1
+            } else {
+                last_bit_info.ending_array_index_exc + num_allocate
+            },
+        };
+
+        bit_info
+
+    }
+    
+}
+
+#[cfg(test)]
+mod test {
+    use crate::BitInfo;
+    use crate::Construct;
+    use crate::BitPack;
+    use crate::BitType;
+    
+    #[test]
+    fn insert_data_one_data() {
+        let mut bit_pack : BitPack = BitPack::new();
+        bit_pack.set("Hello", 4, 7);
+        
+        assert_eq!(bit_pack.get_bit_info(0), &BitInfo {
+            bit_type: BitType::U8(0),
+            starting_bit_position_inc: 0,
+            ending_bit_position_exc: 5,
+            starting_array_index_inc: 0,
+            ending_array_index_exc: 1
+        });
+    }
+
+    #[test]
+    fn insert_data_two_datas() {
+        let mut bit_pack : BitPack = BitPack::new();
+        bit_pack.set("Hello", 4, 7);
+        
+        assert_eq!(bit_pack.get_bit_info(0), &BitInfo {
+            bit_type: BitType::U8(0),
+            starting_bit_position_inc: 0,
+            ending_bit_position_exc: 5,
+            starting_array_index_inc: 0,
+            ending_array_index_exc: 1
+        });
+
+        bit_pack.set("World", 4, 6);
+
+        assert_eq!(bit_pack.get_bit_info(1), &BitInfo {
+            bit_type: BitType::U8(0),
+            starting_bit_position_inc: 5,
+            ending_bit_position_exc: 0,
+            starting_array_index_inc: 0,
+            ending_array_index_exc: 1
+        });
+    }
+
+    #[test]
+    fn insert_data_three_datas() {
+        let mut bit_pack : BitPack = BitPack::new();
+        bit_pack.set("Hello", 4, 7);
+        
+        assert_eq!(bit_pack.get_bit_info(0), &BitInfo {
+            bit_type: BitType::U8(0),
+            starting_bit_position_inc: 0,
+            ending_bit_position_exc: 5,
+            starting_array_index_inc: 0,
+            ending_array_index_exc: 1
+        });
+
+        bit_pack.set("World", 4, 6);
+
+        assert_eq!(bit_pack.get_bit_info(1), &BitInfo {
+            bit_type: BitType::U8(0),
+            starting_bit_position_inc: 5,
+            ending_bit_position_exc: 0,
+            starting_array_index_inc: 0,
+            ending_array_index_exc: 1
+        });
+
+        bit_pack.set("Zack", 4, 3);
+
+        assert_eq!(bit_pack.get_bit_info(2), &BitInfo {
+            bit_type: BitType::U8(0),
+            starting_bit_position_inc: 0,
+            ending_bit_position_exc: 5,
+            starting_array_index_inc: 1,
+            ending_array_index_exc: 2
+        });
+    }
+
+    #[test]
+    fn insert_data_four_datas() {
+        let mut bit_pack : BitPack = BitPack::new();
+        bit_pack.set("Hello", 4, 7);
+        
+        assert_eq!(bit_pack.get_bit_info(0), &BitInfo {
+            bit_type: BitType::U8(0),
+            starting_bit_position_inc: 0,
+            ending_bit_position_exc: 5,
+            starting_array_index_inc: 0,
+            ending_array_index_exc: 1
+        });
+
+        bit_pack.set("World", 4, 6);
+
+        assert_eq!(bit_pack.get_bit_info(1), &BitInfo {
+            bit_type: BitType::U8(0),
+            starting_bit_position_inc: 5,
+            ending_bit_position_exc: 0,
+            starting_array_index_inc: 0,
+            ending_array_index_exc: 1
+        });
+
+        bit_pack.set("Zack", 4, 3);
+
+        assert_eq!(bit_pack.get_bit_info(2), &BitInfo {
+            bit_type: BitType::U8(0),
+            starting_bit_position_inc: 0,
+            ending_bit_position_exc: 5,
+            starting_array_index_inc: 1,
+            ending_array_index_exc: 2
+        });
+
+        bit_pack.set("Markd", 5, 30);
+
+        assert_eq!(bit_pack.get_bit_info(2), &BitInfo {
+            bit_type: BitType::U8(0),
+            starting_bit_position_inc: 5,
+            ending_bit_position_exc: 1,
+            starting_array_index_inc: 1,
+            ending_array_index_exc: 3
+        });
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,41 @@ fn main () {
     bit_pack.set("World", 4, 6);
     
     
-    //0b[0110][0111]
-    
-    //0b0000_0111
-    //OR  0b0000_0110
-//-----------------
-    //0b0000_0111
-// 0000_0000_0110
-    bit_pack.set("Zack", 4, 6);
+    //0b[0110][0111]      
+    //[0] 103
 
-    //0b[0000][0111]_0b[0110][0111]
-    
-    //0b0000_0111
-    //OR  0b0000_0110
-//-----------------
-    //0b0000_0111
-// 0000_0000_0110
+    //starting bit = 0
+    //ending bit = 5
+    //starting arr = 0
+    //ending arr = 1
 
+    //starting bit = 5
+    //ending bit = 0
+    //starring arr =0
+    //ending arr = 1
+
+    bit_pack.set("Zack", 4, 3);
+    // 0b0000_[0011]_0b[0110][0111]
+    //[0] 103
+    //[1] 3
+
+    //starting bit = 0
+    //ending bit = 5
+    //starting arr = 1
+    //ending arr = 2
+
+    bit_pack.set("Markd", 5, 30);
+
+    println!("Done!");
+    // 0b0000_000[1_0b1110]_[0011]_0b[0110][0111]
+    // [0] 103
+    // [1] 227
+    // 0b0000_0011_0b01100111
+    // 0b
+    // [2] 1
+
+    //starting bit = 5
+    //ending bit = 1
+    //starting arr = 1
+    //ending arr = 3
 }
